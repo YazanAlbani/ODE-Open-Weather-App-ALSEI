04e13ef7921b57282bf0406c9fc277c37c43d7fa
Johannes Seitz <ic22b086@technikum-wien.at>
2024-01-07 13:35:29 +0100

    trying to locally fix git issues

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,14 @@
 package org.example;
-
+import Networking.APICall;
+import Networking.APIgenerateURL;
 
 public class Main {
     public static void main(String[] args) {
-
+        APIgenerateURL generateURL = new APIgenerateURL();
+        APICall firstCall = new APICall(generateURL.getURL());
+        String readText;
+        readText = firstCall.getTextReadfromCall();
+        System.out.printf("bis hier");
+        System.out.printf(readText);
     }
 }
